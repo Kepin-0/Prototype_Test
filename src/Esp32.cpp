ba98b391b0694ecf6684feb88f32abf6e5ee605c
Kepin-0 <aurajakti262gmail.com>
2026-09-15 15:07:10 +0700

    tambahan code lcd & visualnya

--- src/Esp32.cpp
+++ src/Esp32.cpp
@@ -1,23 +1,52 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 12  // GPIO4
+#define ONE_WIRE_BUS 12
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+LiquidCrystal_I2C lcd(0x27, 20, 4);  // LCD 20x4
+
 void setup() {
+
   Serial.begin(115200);
+
   sensors.begin();
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Suhu Monitor");
+
+  delay(1000);
 }
 
 void loop() {
+
   sensors.requestTemperatures();
+
   float tempC = sensors.getTempCByIndex(0);
-  
-  Serial.print("Temperature: ");
+
+  // Serial Monitor
+  Serial.print("Suhu: ");
   Serial.print(tempC);
-  Serial.println(" °C");
-  
+  Serial.println(" C");
+
+  // LCD
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("SUHU MONITOR");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Suhu : ");
+
+  lcd.print(tempC);
+  lcd.print(" C");
+
   delay(1000);
 }
